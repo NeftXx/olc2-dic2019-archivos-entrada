27ede3af1a8b3425c58009c168b0efc94f326177
Ronald Berduo <ronaldnef1996@gmail.com>
2019-12-08 00:24:26 -0600

    vector de vectores

--- Cadenas/cadenas.r
+++ Cadenas/cadenas.r
@@ -8,4 +8,24 @@ zro main() {
   _imp(hola);
   _imp("\n");
   _imp(mensaje, s1);
+
+  char lenguajes[][] = {"java", "pascal", "javascript", "python", "c#", "c++", "c"};
+
+  _imp("%s \n", lenguajes[0]);
+  _imp("%s \n", lenguajes[1]);
+  _imp("%s \n", lenguajes[2]);
+  _imp("%s \n", lenguajes[3]);
+  _imp("%s \n", lenguajes[4]);
+  _imp("%s \n", lenguajes[5]);
+  _imp("%s \n", lenguajes[6]);
+
+  char lenguajes2[7][11] = {"java", "pascal", "javascript", "python", "c#", "c++", "c"};
+
+  _imp("%s \n", lenguajes2[0]);
+  _imp("%s \n", lenguajes2[1]);
+  _imp("%s \n", lenguajes2[2]);
+  _imp("%s \n", lenguajes2[3]);
+  _imp("%s \n", lenguajes2[4]);
+  _imp("%s \n", lenguajes2[5]);
+  _imp("%s \n", lenguajes2[6]);
 }
