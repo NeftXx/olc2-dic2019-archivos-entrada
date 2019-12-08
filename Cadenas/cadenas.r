cf946fa6fc3de72bb9b1a5828ad8c9c5d0988bb3
Ronald Berduo <ronaldnef1996@gmail.com>
2019-12-07 20:23:59 -0600

    mensajes

--- Cadenas/cadenas.r
+++ Cadenas/cadenas.r
@@ -1,3 +1,11 @@
 zro main() {
-  _imp("Hola prro");
+  char hola[] = "Hola perro";
+  char s2[] = "Codigo R";
+  char s1[];
+  _copi(s1, s2);
+  char mensaje[] = "Este archivo contiene %s \n";
+
+  _imp(hola);
+  _imp("\n");
+  _imp(mensaje, s1);
 }
