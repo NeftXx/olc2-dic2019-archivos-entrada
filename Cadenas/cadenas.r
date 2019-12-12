272a2cb92d4a836ed9c82a6debfd342695e53253
Ronald Berduo <ronaldnef1996@gmail.com>
2019-12-12 16:29:51 -0600

    Expresiones

--- Cadenas/cadenas.r
+++ Cadenas/cadenas.r
@@ -1,15 +1,15 @@
 zro main() {
-  char hola[] = "Hola perro";
-  char s2[] = "Codigo R";
-  char s1[];
-  _copi(s1, s2);
-  char mensaje[] = "Este archivo contiene %s \n";
+  chr hola[] = "Hola perro";
+  chr s2[] = "Codigo R";
+  chr s1[];
+  s1 = s2;
+  chr mensaje[] = "Este archivo contiene %s \n";
 
   _imp(hola);
   _imp("\n");
   _imp(mensaje, s1);
 
-  char lenguajes[][] = {"java", "pascal", "javascript", "python", "c#", "c++", "c"};
+  chr lenguajes[][] = {"java", "pascal", "javascript", "python", "c#", "c++", "c"};
 
   _imp("%s \n", lenguajes[0]);
   _imp("%s \n", lenguajes[1]);
@@ -19,7 +19,7 @@ zro main() {
   _imp("%s \n", lenguajes[5]);
   _imp("%s \n", lenguajes[6]);
 
-  char lenguajes2[7][11] = {"java", "pascal", "javascript", "python", "c#", "c++", "c"};
+  chr lenguajes2[7][11] = {"java", "pascal", "javascript", "python", "c#", "c++", "c"};
 
   _imp("%s \n", lenguajes2[0]);
   _imp("%s \n", lenguajes2[1]);
