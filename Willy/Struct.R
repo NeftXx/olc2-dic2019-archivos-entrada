d38df71846aa5a7da4ce540509616a09ed61de29
Ronald Berduo <ronaldnef1996@gmail.com>
2019-12-17 14:29:47 -0600

    reserva

--- Willy/Struct.R
+++ Willy/Struct.R
@@ -2,33 +2,32 @@ fusion persona{
     ent edad;
     chr nombre;
     persona siguiente;
-}
+};
 
 zro main() {
-    persona per;
-    per=3;
+    persona per = _reservar(_pesode(persona));
     per.edad=5;
 
-    per.siguiente=3;
+    per.siguiente=_reservar(_pesode(persona));
     per.siguiente.edad=7;
-    per.siguiente.siguiente=3;
+    per.siguiente.siguiente=_reservar(_pesode(persona));
     per.siguiente.siguiente.siguiente=per;
 
     per.edad=9;
 
-    _imp(per.edad);                //9
-    _imp('\n');
-    _imp(per.siguiente.edad);      //7
-    _imp('\n');
-    _imp(per.siguiente.siguiente.edad);  //0
-    _imp('\n');
-    _imp(per.siguiente.siguiente.siguiente.edad);  //9
+    _imp("%e", per.edad);                //9
+    _imp("\n");
+    _imp("%e", per.siguiente.edad);      //7
+    _imp("\n");
+    _imp("%e", per.siguiente.siguiente.edad);  //0
+    _imp("\n");
+    _imp("%e", per.siguiente.siguiente.siguiente.edad);  //9
 
     per.siguiente.siguiente.siguiente.siguiente=per.siguiente.siguiente.siguiente.siguiente.siguiente;
 
-    _imp('\n');
-    _imp(per.siguiente.siguiente.siguiente.siguiente.edad);              //9
-    _imp('\n');
-    _imp(per.siguiente.siguiente.siguiente.siguiente.siguiente.edad);   //0
+    _imp("\n");
+    _imp("%e", per.siguiente.siguiente.siguiente.siguiente.edad);              //9
+    _imp("\n");
+    _imp("%e", per.siguiente.siguiente.siguiente.siguiente.siguiente.edad);   //0
 
 }
