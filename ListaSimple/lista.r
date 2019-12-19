d9f5ea45ef7fb44572bb15e714b3108d2c990622
Ronald Berduo <ronaldnef1996@gmail.com>
2019-12-18 18:00:26 -0600

    Lista Simple 1.1

--- ListaSimple/lista.r
+++ ListaSimple/lista.r
@@ -15,13 +15,24 @@ zro insertar(Lista lista, ent dato) {
   lista.inicio = nuevo;
 }
 
+zro imprimirLista(Lista lista) {
+  Nodo temp = lista.inicio;
+  while(temp <> nlo) {
+    _imp("%e ", temp.dato);
+    temp = temp.sig;
+  }
+  _imp("\n");
+}
+
 zro main() {
+  _imp("================= LISTA SIMPLE =================\n");
   Lista lista = _reservar(_pesode(Lista));
   insertar(lista, 10);
-  /*insertar(lista, 20);
+  insertar(lista, 20);
   insertar(lista, 30);
   insertar(lista, 1);
   insertar(lista, 40);
-  insertar(lista, 56);*/
+  insertar(lista, 56);
   // 56 -> 40 -> 1 -> 30 -> 20 -> 10
+  imprimirLista(lista);
 }
